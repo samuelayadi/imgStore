3afa31f7d57fc21c31d362541f65a3c656bfe88a
samuelayadi <samuelayadi4999@gmail.com>
2023-09-20 20:20:28 +0000

    Made changes in the loading state

--- src/components/Gallery/ImageCard.jsx
+++ src/components/Gallery/ImageCard.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 
 const ImageCard = ({ search }) => {
   const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading]= useState(true)
   const apiKey = "LdmawpiugbPaHgUah1ugD1YI9rSfyohIL4lbyXTP9nVtfiOV3SkEFh5k";
   const apiUrl = "https://api.pexels.com/v1/search";
   const isGallery = location.pathname === "/gallery";
@@ -26,8 +27,10 @@ const ImageCard = ({ search }) => {
         .then((response) => {
           const photos = response.data.photos;
           setImages(photos);
+          setIsLoading(false)
         })
         .catch((error) => {
+          setIsLoading(false);
           console.error(
             "Error fetching default images from Pexels API:",
             error
@@ -44,9 +47,11 @@ const ImageCard = ({ search }) => {
         .then((response) => {
           const photos = response.data.photos;
           setImages(photos);
+          setIsLoading(false);
         })
         .catch((error) => {
           console.error("Error fetching data from Pexels API:", error);
+          setIsLoading(false);
         });
     }
 
@@ -61,12 +66,14 @@ const ImageCard = ({ search }) => {
         .then((response) => {
           const photos = response.data.photos;
           setImages(photos);
+          setIsLoading(false);
         })
         .catch((error) => {
           console.error(
             "Error fetching default images from Pexels API:",
             error
           );
+          setIsLoading(false);
         });
     }
   }, [search]);
@@ -88,27 +95,36 @@ const ImageCard = ({ search }) => {
     setImages(updatedImages);
   };
 
-
   return (
     <>
-      {images.map((image, index) => (
-        <div
-          key={image.id}
-          draggable="true"
-          className="w-[200px] xs:w-auto sm:w-[200px] h-[300px] mx-auto md:w-[230px]"
-          onDragStart={(e) => handleDragStart(e, index)}
-          onDragOver={handleDragOver}
-          onDrop={(e) => handleDrop(e, index)}
-        >
-          <Link to={image.url}>
-            <img
-              src={image.src.medium}
-              alt={`Image ${image.alt}`}
-              className="w-full h-full object-cover rounded-lg"
-            />
-          </Link>
+      {isLoading ? (
+        <div>
+          <div className="flex h-screen w-screen items-center justify-center">
+            <div className="border-t-transparent border-solid animate-spin  rounded-full border-primary border-8 h-64 w-64"></div>
+          </div>
         </div>
-      ))}
+      ) : (
+        <>
+          {images.map((image, index) => (
+            <div
+              key={image.id}
+              draggable="true"
+              className="w-[200px] xs:w-auto sm:w-[200px] h-[300px] mx-auto md:w-[230px]"
+              onDragStart={(e) => handleDragStart(e, index)}
+              onDragOver={handleDragOver}
+              onDrop={(e) => handleDrop(e, index)}
+            >
+              <Link to={image.url}>
+                <img
+                  src={image.src.medium}
+                  alt={`Image ${image.alt}`}
+                  className="w-full h-full object-cover rounded-lg"
+                />
+              </Link>
+            </div>
+          ))}
+        </>
+      )}
     </>
   );
 };
